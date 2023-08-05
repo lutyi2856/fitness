a62f27216dbb408a808db87885318117b8a8a11b
Сергей Сычёв <lutyi2856@gmail.com>
2023-08-05 20:50:45 +0300

    добавляет состояние для элементов

--- source/js/slider-comments.js
+++ source/js/slider-comments.js
@@ -3,8 +3,8 @@ import Swiper from './vendor/swiper';
 export function initializeSwiperComments() {
   const swiperComments = new Swiper('.swiper-comments', {
     slidesPerView: 1,
+    rtl: false,
     direction: 'horizontal',
-    loop: true,
     navigation: {
       nextEl: '.swiper-comments-button-next',
       prevEl: '.swiper-comments-button-prev',
